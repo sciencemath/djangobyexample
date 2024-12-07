1ca3b1640e4998ebead20fb42af0cdab74533b84
sciencemath <matrixmachinelearning@gmail.com>
2024-12-06 23:44:53 -0500

    abstract forms

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path(
@@ -16,6 +16,7 @@ urlpatterns = [
       name='logout'
     ),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls')),
 ]
 
 if settings.DEBUG:
