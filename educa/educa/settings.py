a07b2138441d60de823a836257d5f8aa508ec341
sciencemath <matrixmachinelearning@gmail.com>
2024-12-08 16:25:35 -0500

    building RESTful API with DRF, restricting access to certain views, used the requests library to consume the API from external Python script

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'embed_video',
     'debug_toolbar',
     'redisboard',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -145,3 +146,9 @@ INTERNAL_IPS = [
 CACHE_MIDDLEWARE_ALIAS = 'default'
 CACHE_MIDDLEWARE_SECONDS = 60 * 15
 CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
+
+REST_FRAMEWORK = {
+  'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+  ]
+}
